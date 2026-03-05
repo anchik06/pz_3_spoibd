3db00fab00845c91463eef5d8bb21d794670ace2
anchik06 <an_sasha_0603@mail.ru>
2026-03-05 21:52:57 +0300

    Update pz3.py

--- pz3.py
+++ pz3.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
 cnx = mysql.connector.connect(
-    host="srv221-h-st.jino.ru",
-    database="j30084097_137",
-    user="j30084097_137",
-    password="Gruppa137")
+    host="",
+    database="",
+    user="",
+    password="")
 
 
 cur = cnx.cursor()
@@ -15,3 +15,4 @@ row = cur.fetchone()
 print("Current date is: {0}".format(row[0]))
 
 cnx.close()
+
